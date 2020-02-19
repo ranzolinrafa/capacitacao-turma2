5fbd74c44fdd3d5a73373f39d1f71fcbf529b5e9
leorauber <leorauber@hotmail.com>
2020-02-19 18:24:08 -0300

    entrega
    entrega exercicio 1 capacitacao 2

--- exercicio-1/main.js
+++ exercicio-1/main.js
@@ -1,1 +1,44 @@
 // Javascript File
+
+var login = document.getElementById('btn_login');
+var forms = document.getElementsByClassName('form_login');
+var invalid = document.getElementById('invalid');
+var checkbox = document.getElementById('checkbox_lembrar');
+var chk_hidden = document.getElementById('chk_hidden');
+
+
+for (i = 0; i < forms.length; i++) {
+	forms[i].addEventListener('keyup', function(event){
+		event.target.classList.remove('form_login_error');
+		event.target.classList.remove('form_login_sucess');
+		invalid.classList.remove('invalid_show');
+	});
+}
+
+login.addEventListener('click', function(event){
+	var flagInvalid = 0;
+	for (i = 0; i < forms.length; i++) {
+		if (forms[i].value.length < 5) {
+			forms[i].classList.add('form_login_error');
+			flagInvalid++;
+		} else {
+			forms[i].classList.add('form_login_sucess');
+		}
+	}
+	if (flagInvalid > 0) {
+		invalid.classList.add('invalid_show');	
+	}
+});
+
+var flagChkActive = 0;
+checkbox.addEventListener('click', function(event){
+	if (!flagChkActive) {
+		checkbox.classList.add('div_chk_active');
+		flagChkActive = 1;
+		chk_hidden.setAttributeNode(document.createAttribute("checked"));
+	} else {
+		checkbox.classList.remove('div_chk_active');
+		flagChkActive = 0;
+		chk_hidden.removeAttribute("checked");
+	}
+});
